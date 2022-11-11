d6247ac9105e0346127d08ad2c53493b6b356629
viztech-systems <116189264+viztech-systems@users.noreply.github.com>
2022-11-11 13:31:31 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-//   env:{
-//     WEBSITE_URL : process.env.WEBSITE_URL,
-//     MONGODB_URL : process.env.MONGODB_URL,
-//     JWT_SECRET_KEY : process.env.JWT_SECRET_KEY,
-//     MY_AWS_ACCESS_KEY_NAME : process.env.MY_AWS_ACCESS_KEY_NAME,
-//     MY_AWS_SECRET_ACCESS_KEY_NAME : process.env.MY_AWS_SECRET_ACCESS_KEY_NAME,
-//     MY_AWS_BUCKET_NAME : process.env.MY_AWS_BUCKET_NAME,
-//     MY_AWS_BUCKET_REGION : process.env.MY_AWS_BUCKET_REGION
-//   }
+  /* config options here */
 }
 
 module.exports = nextConfig
